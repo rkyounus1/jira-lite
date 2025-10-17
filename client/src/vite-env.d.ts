221bcf000ddca1eb9b7c2563f54f41ff67a34acf
rkyounus1 <rkyounus1@example.com>
2025-10-18 01:16:51 +0530

    fix: added URGENT priority to TaskPriorityEnum

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,12 +1,1 @@
-/// <reference types="node" />
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
-export default defineConfig({
-  plugins: [react()],
-  resolve: { alias: { '@': path.resolve(__dirname, './src') } },
-});
+/// <reference types="vite/client" /> 
