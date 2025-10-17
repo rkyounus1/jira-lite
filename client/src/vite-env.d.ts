9cb3becfb9af09889323a1c13ad043e517fd2f58
rkyounus1 <rkyounus1@example.com>
2025-10-18 01:10:41 +0530

    initial

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,1 +1,12 @@
-/// <reference types="vite/client" />
+/// <reference types="node" />
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: { alias: { '@': path.resolve(__dirname, './src') } },
+});
